0a2132c2d8407c9b671af148d0f0d6d28914a462
Stephan König <s.konig@outlook.de>
2023-04-24 00:13:31 +0200

    TableViewController now shows data from Realm Database.

--- Horizonte Fahrtenbuch/List Views/contactsTableViewController.swift
+++ Horizonte Fahrtenbuch/List Views/contactsTableViewController.swift
@@ -10,21 +10,79 @@ import RealmSwift
 
 
 
+class Cell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String!) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
 
+    required init(coder: NSCoder) {
+        fatalError("NSCoding not supported")
+    }
+}
 
 class contactsTableViewController: UITableViewController {
     
     // MARK: Initializing Realm
     
     let realm = try! Realm()
-
-    // MARK: - Table view data source
-
-    /*override func numberOfSections(in tableView: UITableView) -> Int {
+    let results = try! Realm().objects(clients.self).sorted(byKeyPath: "client")
+        var notificationToken: NotificationToken?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        let count = realm.objects(clients.self).count
-        return count
-    }*/
+        
+        /*if realm.objects(clients.self).count < 1 {
+            let alert = UIAlertController(title: "GPS Zugriff verweigert", message: "Du hast noch keine Kundendaten gespeichert", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+                
+                switch action.style{
+                    
+                    case .default:
+                    print("default")
+                    
+                    case .cancel:
+                    print("cancel")
+                    
+                    case .destructive:
+                    print("destructive")
+                    
+                @unknown default:
+                    print("Unknown Fault")
+                }
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }*/
+        
+        setupUI()
+        
+        // Set results notification block
+        self.notificationToken = results.observe { (changes: RealmCollectionChange) in
+            switch changes {
+            case .initial:
+                // Results are now populated and can be accessed without blocking the UI
+                self.tableView.reloadData()
+            case .update(_, let deletions, let insertions, let modifications):
+                // Query results have changed, so apply them to the TableView
+                self.tableView.beginUpdates()
+                self.tableView.insertRows(at: insertions.map { IndexPath(row: $0, section: 0) }, with: .automatic)
+                self.tableView.deleteRows(at: deletions.map { IndexPath(row: $0, section: 0) }, with: .automatic)
+                self.tableView.reloadRows(at: modifications.map { IndexPath(row: $0, section: 0) }, with: .automatic)
+                self.tableView.endUpdates()
+            case .error(let err):
+                // An error occurred while opening the Realm file on the background worker thread
+                fatalError("\(err)")
+            }
+        }
+    }
+    
+    // MARK: Setting Up User Interface
+        func setupUI() {
+            tableView.register(Cell.self, forCellReuseIdentifier: "clientCell")
+
+            self.title = "Kunden"
+        }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let count = realm.objects(clients.self).count
@@ -34,11 +92,23 @@ class contactsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "clientCell", for: indexPath)
+        
+        let object = results[indexPath.row]
+        cell.textLabel?.text = object.client?.description
+        cell.detailTextLabel?.text = object.city?.description
+        cell.textLabel?.textColor = UIColor.init(red: 156/255, green: 199/255, blue: 105/255, alpha: 1.0)
 
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
 
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+                realm.beginWrite()
+                realm.delete(results[indexPath.row])
+                try! realm.commitWrite()
+            }
+        }
 
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "Kunden"
