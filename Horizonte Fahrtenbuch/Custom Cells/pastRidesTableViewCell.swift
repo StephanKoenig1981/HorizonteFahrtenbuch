4cf19db0c78cf42d28e1d40381f5494600840116
Stephan König <s.konig@outlook.de>
2023-04-26 23:02:32 +0200

    Preparation work for custom cells

---
+++ Horizonte Fahrtenbuch/Custom Cells/pastRidesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  pastRidesTableViewCell.swift
+//  Horizonte Fahrtenbuch
+//
+//  Created by Stephan König on 26.04.23.
+//
+
+import UIKit
+
+class pastRidesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
