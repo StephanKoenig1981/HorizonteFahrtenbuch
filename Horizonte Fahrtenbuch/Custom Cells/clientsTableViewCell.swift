b389f42ce5c78a368068cbc1fb37dc1f0c5bed64
Stephan König <s.konig@outlook.de>
2023-04-29 13:56:25 +0200

    Added class for custom clients table view cell

---
+++ Horizonte Fahrtenbuch/Custom Cells/clientsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  clientsTableViewCell.swift
+//  Horizonte Fahrtenbuch
+//
+//  Created by Stephan König on 29.04.23.
+//
+
+import UIKit
+
+class clientsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
