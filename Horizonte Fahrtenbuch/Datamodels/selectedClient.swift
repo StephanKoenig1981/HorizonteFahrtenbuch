4c1119536e93274adf9474eb1958f77ab091d34f
Stephan König <s.konig@outlook.de>
2023-05-01 14:28:22 +0200

    Created new datamodel for selected client.

---
+++ Horizonte Fahrtenbuch/Datamodels/selectedClient.swift
@@ -0,0 +1,17 @@
+//
+//  selectedClient.swift
+//  Horizonte Fahrtenbuch
+//
+//  Created by Stephan König on 01.05.23.
+//
+
+import Foundation
+import RealmSwift
+
+// MARK: THIS CLASS MODEL IS TESTING ONLY
+
+class selectedClient: Object {
+    
+    @objc dynamic var selectedClientName: String?
+
+}
