f87ef5a4bf1ad2894f6811c3632041f387d2547a
Stephan König <s.konig@outlook.de>
2023-05-05 22:43:52 +0200

    Update work

--- Horizonte Fahrtenbuch/Helper Classes/settingsViewController.swift
+++ Horizonte Fahrtenbuch/Helper Classes/settingsViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import CloudKit
+import RealmSwift
 
 class settingsViewController: UIViewController {
+    
+    struct DocumentsDirectory {
+        static let localDocumentsURL = FileManager.default.urls(for: FileManager.SearchPathDirectory.documentDirectory, in: .userDomainMask).last!
+        static let iCloudDocumentsURL = FileManager.default.url(forUbiquityContainerIdentifier: nil)?.appendingPathComponent("Documents")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+    }
+    
+    func isCloudEnabled() -> Bool {
+        if DocumentsDirectory.iCloudDocumentsURL != nil { return true }
+        else { return false }
+    }
+    
+    
+    @IBAction func uploadPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func donwloadPressed(_ sender: Any) {
     }
 }
