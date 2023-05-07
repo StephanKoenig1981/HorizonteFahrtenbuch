01b80d2ea5cd007bb2f912c2e3b09efbb29bdcaa
Stephan König <s.konig@outlook.de>
2023-05-08 00:24:32 +0200

    Initial work on backing up the database.

--- Horizonte Fahrtenbuch/Helper Classes/settingsViewController.swift
+++ Horizonte Fahrtenbuch/Helper Classes/settingsViewController.swift
@@ -11,25 +11,89 @@ import RealmSwift
 
 class settingsViewController: UIViewController {
     
-    struct DocumentsDirectory {
-        static let localDocumentsURL = FileManager.default.urls(for: FileManager.SearchPathDirectory.documentDirectory, in: .userDomainMask).last!
-        static let iCloudDocumentsURL = FileManager.default.url(forUbiquityContainerIdentifier: nil)?.appendingPathComponent("Documents")
-    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    func isCloudEnabled() -> Bool {
-        if DocumentsDirectory.iCloudDocumentsURL != nil { return true }
-        else { return false }
+    
+    private func retrieveLocalRealmURL() -> URL {
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentaryDirectory = urls[0]
+        let realmURL = documentaryDirectory.appendingPathComponent("default.realm");
+        
+        return realmURL
+    }
+
+    private func backupRealmToiCloudDrive() {
+        let backgroundQueue = DispatchQueue.global(qos: .background)
+        
+        backgroundQueue.async {
+            guard
+                let ubiquityURL = FileManager.default.url(forUbiquityContainerIdentifier: nil)
+            else {
+                return
+            }
+                
+            //let iCloudDriveURL = ubiquityURL.appendingPathComponent("Documents")
+            let iCloudRealmURL = ubiquityURL.appendingPathComponent("default.realm")
+            
+            let fileExists = FileManager.default.fileExists(atPath: iCloudRealmURL.path, isDirectory: nil)
+            
+            func copy() {
+                let localRealmURL = self.retrieveLocalRealmURL()
+                
+                do {
+                    try FileManager.default.copyItem(at: localRealmURL, to: iCloudRealmURL)
+                    print ("Die Datei wurde kopiert")
+                } catch {
+                    print (error.localizedDescription)
+                }
+            }
+            
+            if fileExists {
+                self.deleteExistedFile(iCloudRealmURL)
+                copy()
+            } else {
+                do {
+                    try FileManager.default.createDirectory(at: iCloudRealmURL, withIntermediateDirectories: true, attributes: nil)
+                    copy()
+                } catch {
+                    print (error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+
+    private func deleteExistedFile(_ url: URL) {
+        let fileCoordinator = NSFileCoordinator(filePresenter: nil)
+        
+        fileCoordinator.coordinate(writingItemAt: url, options: .forDeleting, error: nil) { deleteURL in
+            do {
+                let fileExists = FileManager.default.fileExists(atPath: deleteURL.path, isDirectory: nil)
+                
+                if fileExists {
+                    try FileManager.default.removeItem(at: deleteURL)
+                }
+            } catch {
+                print (error.localizedDescription)
+            }
+        }
+    }
+    
+    private func restoreFromiCloudDrive() {
     }
     
     
     @IBAction func uploadPressed(_ sender: Any) {
-    }
+        backupRealmToiCloudDrive()
+        }
+    
     
     
     @IBAction func donwloadPressed(_ sender: Any) {
+        restoreFromiCloudDrive()
     }
 }
