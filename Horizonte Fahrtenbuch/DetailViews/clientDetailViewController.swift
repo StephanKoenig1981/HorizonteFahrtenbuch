82002a18dd9011907d8664227fe0173c32f650b7
Stephan König <s.konig@outlook.de>
2023-05-15 13:04:04 +0200

    Work on callouts and UI fixes.

--- Horizonte Fahrtenbuch/DetailViews/clientDetailViewController.swift
+++ Horizonte Fahrtenbuch/DetailViews/clientDetailViewController.swift
@@ -10,7 +10,7 @@ import RealmSwift
 import CoreLocation
 import MapKit
 
-class clientDetailViewController: UIViewController, CLLocationManagerDelegate {
+class clientDetailViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     var locationManager = CLLocationManager()
     var clientName: String?
@@ -19,23 +19,46 @@ class clientDetailViewController: UIViewController, CLLocationManagerDelegate {
     var latitude: Double = 0
     var longitude: Double = 0
     
-
+    
     @IBOutlet weak var clientDetailMapView: MKMapView!
     
     override func viewDidLoad() {
-            super.viewDidLoad()
+        super.viewDidLoad()
         
-            clientDetailMapView.layer.cornerRadius = 20
-
-            let initialLocation = CLLocation(latitude: latitude, longitude: longitude)
-            let regionRadius: CLLocationDistance = 1000
-            let region = MKCoordinateRegion(center: initialLocation.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
-            clientDetailMapView.setRegion(region, animated: true)
-            
-            let annotation = CustomAnnotation(coordinate: CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude), title: self.clientName, subtitle: "\(self.clientStreet ?? ""), \(self.clientCity ?? "")")
-            clientDetailMapView.addAnnotation(annotation)
-        }
-
-        //...
+        clientDetailMapView.delegate = self
+        
+        clientDetailMapView.layer.cornerRadius = 20
+        
+        let initialLocation = CLLocation(latitude: latitude, longitude: longitude)
+        let regionRadius: CLLocationDistance = 500
+        let region = MKCoordinateRegion(center: initialLocation.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        clientDetailMapView.setRegion(region, animated: true)
+        
+        let annotation = CustomAnnotation(coordinate: CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude), title: self.clientName, subtitle: "\(self.clientStreet ?? ""), \(self.clientCity ?? "")")
+        clientDetailMapView.addAnnotation(annotation)
+        
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let customAnnotation = annotation as? CustomAnnotation else { return nil }
+        
+        let identifier = "customAnnotation"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: customAnnotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            annotationView?.tintColor = .systemOrange
+        } else {
+            annotationView?.annotation = customAnnotation
+        }
+        
+        // Show callout bubble and set title
+        annotationView?.isSelected = false
+        annotationView?.glyphText = customAnnotation.title
+        
+        return annotationView
+    }
+}
+    
 
