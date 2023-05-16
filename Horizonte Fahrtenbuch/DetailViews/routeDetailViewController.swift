6ffa5eda30736b9c5127d7cc8fe501d2c85fb5bb
Stephan König <s.konig@outlook.de>
2023-05-16 23:19:56 +0200

    Debugging

--- Horizonte Fahrtenbuch/DetailViews/routeDetailViewController.swift
+++ Horizonte Fahrtenbuch/DetailViews/routeDetailViewController.swift
@@ -8,62 +8,69 @@
 import UIKit
 import MapKit
 
-struct Polyline: Codable {
-    var points: [PointData]
+class routeDetailViewController: UIViewController {
 
-    init(_ polyline: MKPolyline) {
-        self.points = (0 ..< polyline.pointCount).map {
-            let mapPoint = polyline.points()[$0]
-            let coordinate = mapPoint.coordinate
-            return PointData(coordinate)
-        }
-    }
-}
-
-class PointData: Codable {
-    var latitude: Double
-    var longitude: Double
-
-    init(_ coordinate: CLLocationCoordinate2D) {
-        self.latitude = coordinate.latitude
-        self.longitude = coordinate.longitude
-    }
-}
-
-class routeDetailViewController: UIViewController, MKMapViewDelegate {
-
-    @IBOutlet weak var routeDetailMapView: MKMapView!
     var encodedPolyline: Data?
-
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var routeDetailMapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Decode the encodedPolyline object
-        guard let encodedPolyline = encodedPolyline,
-              let polyline = try? JSONDecoder().decode(Polyline.self, from: encodedPolyline) else {
-            print("Error decoding polyline")
+        
+        routeDetailMapView.delegate = self
+        routeDetailMapView.layer.cornerRadius = 20
+        
+        // Check if the encodedPolyline is not nil
+        guard let encodedPolyline = encodedPolyline else {
+            print("There is no encodedPolyline data to decode")
             return
         }
-        print(polyline.points) // Debugging
-
-        // Draw the polyline on the mapView
-        let points = polyline.points.map { CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude) }
-        let mapPolyline = MKPolyline(coordinates: points, count: points.count)
-        routeDetailMapView.addOverlay(mapPolyline)
-        routeDetailMapView.delegate = self
-    }
-
-    // MARK: - MKMapViewDelegate
-
-    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        guard let polyline = overlay as? MKPolyline else {
-            return MKOverlayRenderer(overlay: overlay)
+        
+        // Decode the encodedPolyline data into a Polyline object
+        guard let polyline = try? JSONDecoder().decode(Polyline.self, from: encodedPolyline) else {
+            print("Failed to decode the encodedPolyline")
+            return
         }
-
-        let renderer = MKPolylineRenderer(polyline: polyline)
-        renderer.strokeColor = .blue
-        renderer.lineWidth = 5.0
-        return renderer
+        
+        // Create an array of CLLocationCoordinate2D points from the polyline object
+        var points: [CLLocationCoordinate2D] = []
+        for coordinate in polyline.coordinates {
+            let point = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            points.append(point)
+        }
+        
+        // Create an MKPolyline from the points array and add it to the map
+        let polyLine = MKPolyline(coordinates: points, count: points.count)
+        routeDetailMapView.addOverlay(polyLine)
+        
+        // Set the map region to fit the polyline
+        if let firstPoint = points.first, let lastPoint = points.last {
+            let startRegion = MKCoordinateRegion(center: firstPoint, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            let endRegion = MKCoordinateRegion(center: lastPoint, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            let region = MKCoordinateRegion(center: firstPoint, latitudinalMeters: startRegion.span.latitudeDelta * 2.5 + endRegion.span.latitudeDelta * 2.5, longitudinalMeters: startRegion.span.longitudeDelta * 2.5 + endRegion.span.longitudeDelta * 2.5)
+            routeDetailMapView.setRegion(region, animated: true)
+        }
     }
+}
 
+extension routeDetailViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let polyline = overlay as? MKPolyline {
+            let renderer = MKPolylineRenderer(polyline: polyline)
+            renderer.strokeColor = UIColor.blue
+            renderer.lineWidth = 3.0
+            return renderer
+        }
+        return MKOverlayRenderer()
+    }
+}
+
+struct Polyline: Codable {
+    var coordinates: [Coordinate]
+    
+    struct Coordinate: Codable {
+        var latitude: Double
+        var longitude: Double
+    }
 }
