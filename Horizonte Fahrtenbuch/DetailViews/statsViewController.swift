a837d35d79ed481f4cdf75cd93ba74f67a6d68e6
Stephan König <s.konig@outlook.de>
2023-05-25 00:46:54 +0200

    Version bump.

--- Horizonte Fahrtenbuch/DetailViews/statsViewController.swift
+++ Horizonte Fahrtenbuch/DetailViews/statsViewController.swift
@@ -193,7 +193,7 @@ class statsViewController: UIViewController, MFMailComposeViewControllerDelegate
             //emailText += "\n"
             emailText += "_______________________\n\n"
         }
-        emailText += "Dieser Bericht wurde durch die Horizonte Fahrtenbuch App V1.7.8 generiert."
+        emailText += "Dieser Bericht wurde durch die Horizonte Fahrtenbuch App V1.7.9 generiert."
         
         if MFMailComposeViewController.canSendMail() {
             let mailComposer = MFMailComposeViewController()
