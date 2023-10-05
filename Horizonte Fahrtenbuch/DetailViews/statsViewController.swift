4a545e0de0947f4760c537c4ee4410b12233127b
Stephan König <s.konig@outlook.de>
2023-10-05 16:57:19 +0200

    Minor Bugfix

--- Horizonte Fahrtenbuch/DetailViews/statsViewController.swift
+++ Horizonte Fahrtenbuch/DetailViews/statsViewController.swift
@@ -295,7 +295,7 @@ class statsViewController: UIViewController, MFMailComposeViewControllerDelegate
          }*/
          
          emailText += "<br><br>"
-         emailText += "Dieser Bericht wurde durch die Horizonte Fahrtenbuch App V2.3.4 generiert. - © 2023 Stephan König"
+         emailText += "Dieser Bericht wurde durch die Horizonte Fahrtenbuch App V2.3.5 generiert. - © 2023 Stephan König"
          
          if MFMailComposeViewController.canSendMail() {
              let mailComposer = MFMailComposeViewController()
