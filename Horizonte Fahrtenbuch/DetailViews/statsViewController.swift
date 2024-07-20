e515629f5695ffb2bfcf1688dd430ac1ffe666df
Stephan König <s.konig@outlook.de>
2024-07-21 00:55:30 +0200

    Pod updates

--- Horizonte Fahrtenbuch/DetailViews/statsViewController.swift
+++ Horizonte Fahrtenbuch/DetailViews/statsViewController.swift
@@ -311,7 +311,7 @@ class statsViewController: UIViewController, MFMailComposeViewControllerDelegate
         }
         
          emailText += "<br><br>"
-         emailText += "Dieser Bericht wurde durch die Horizonte Fahrtenbuch App V4.2.0 beta 9 generiert. - © 2023 - 2024 Stephan König (GPL 3.0)"
+         emailText += "Dieser Bericht wurde durch die Horizonte Fahrtenbuch App V4.2.0 beta 10 generiert. - © 2023 - 2024 Stephan König (GPL 3.0)"
          
          if MFMailComposeViewController.canSendMail() {
              let mailComposer = MFMailComposeViewController()
@@ -402,7 +402,7 @@ class statsViewController: UIViewController, MFMailComposeViewControllerDelegate
              }
              
              emailText += "<br><br>"
-             emailText += "Dieser Bericht wurde durch die Horizonte Fahrtenbuch App V4.2.0 beta 9 generiert. - © 2023 - 2024 Stephan König (GPL 3.0)"
+             emailText += "Dieser Bericht wurde durch die Horizonte Fahrtenbuch App V4.2.0 beta 10 generiert. - © 2023 - 2024 Stephan König (GPL 3.0)"
              
              if MFMailComposeViewController.canSendMail() {
                  let mailComposer = MFMailComposeViewController()
