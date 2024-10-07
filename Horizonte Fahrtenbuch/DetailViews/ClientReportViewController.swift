128fb76a11e4afd49205082852d357eed01d64f1
Stephan König <s.konig@outlook.de>
2024-10-07 19:01:18 +0200

    UI work for sending per client reports

--- Horizonte Fahrtenbuch/DetailViews/ClientReportViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ClientReportViewController.swift
-//  Horizonte Fahrtenbuch
-//
-//  Created by Stephan König on 07.10.2024.
-//
-
-import Foundation
---
+++ Horizonte Fahrtenbuch/DetailViews/clientReportViewController.swift
@@ -0,0 +1,29 @@
+//
+//  clientReportViewController.swift
+//  Horizonte Fahrtenbuch
+//
+//  Created by Stephan König on 07.10.2024.
+//
+
+import UIKit
+
+class clientReportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
