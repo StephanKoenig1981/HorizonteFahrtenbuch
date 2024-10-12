743d0d493a6c4e74203913f8f920e7b989c19991
Stephan König <s.konig@outlook.de>
2024-10-12 20:16:44 +0200

    The app can now show overall statistics and send them.

---
+++ Horizonte Fahrtenbuch/DetailViews/detailStatsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  detailStatsViewController.swift
+//  Horizonte Fahrtenbuch
+//
+//  Created by Stephan König on 12.10.2024.
+//
+
+import UIKit
+
+class detailStatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
