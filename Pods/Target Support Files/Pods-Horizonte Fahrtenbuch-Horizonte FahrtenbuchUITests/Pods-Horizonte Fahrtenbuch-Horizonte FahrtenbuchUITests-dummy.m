1260f0bb539101355bb867c92f13601a4a50112e
Stephan König <s.konig@outlook.de>
2025-10-16 21:14:37 +0200

    Update .gitignore und entferne getrackte Xcode-Benutzerdateien

--- Pods/Target Support Files/Pods-Horizonte Fahrtenbuch-Horizonte FahrtenbuchUITests/Pods-Horizonte Fahrtenbuch-Horizonte FahrtenbuchUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Horizonte_Fahrtenbuch_Horizonte_FahrtenbuchUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Horizonte_Fahrtenbuch_Horizonte_FahrtenbuchUITests
-@end
